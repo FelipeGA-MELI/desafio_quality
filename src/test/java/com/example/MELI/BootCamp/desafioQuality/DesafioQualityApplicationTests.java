7bc028705eef7a5289b882cd6a6dcbe8916faf94
Felipe Gusmao <FelipeGA-MELI>
2021-06-10 12:14:54 -0300

    Primeiro commit.

---
+++ src/test/java/com/example/MELI/BootCamp/desafioQuality/DesafioQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MELI.BootCamp.desafioQuality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioQualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
