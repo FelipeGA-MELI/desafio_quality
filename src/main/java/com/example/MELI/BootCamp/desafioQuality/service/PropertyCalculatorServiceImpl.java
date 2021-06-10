c508ec78ea8ce5ba5342779fed61c9529b5a7d69
Felipe Gusmao <FelipeGA-MELI>
2021-06-10 14:45:36 -0300

    US01 finlaizada

--- src/main/java/com/example/MELI/BootCamp/desafioQuality/service/PropertyCalculatorServiceImpl.java
+++ src/main/java/com/example/MELI/BootCamp/desafioQuality/service/PropertyCalculatorServiceImpl.java
@@ -1,13 +1,21 @@
 package com.example.MELI.BootCamp.desafioQuality.service;
 
 import com.example.MELI.BootCamp.desafioQuality.model.Property;
+import com.example.MELI.BootCamp.desafioQuality.model.Room;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class PropertyCalculatorServiceImpl implements PropertyCalculatorService {
     @Override
     public Double calculatePropertyArea(Property property) {
-        return null;
+        List<Room> roomList = property.getProp_rooms();
+        Double propertyArea = 0.0;
+
+        for(Room room: roomList)
+            propertyArea += room.getRoom_length() * room.getRoom_width();
+
+        return propertyArea;
     }
 
     @Override
--- src/main/java/com/example/MELI/BootCamp/desafioQuality/service/RoomCalculatorServiceImpl.java
+++ src/main/java/com/example/MELI/BootCamp/desafioQuality/service/RoomCalculatorServiceImpl.java
@@ -2,7 +2,9 @@ package com.example.MELI.BootCamp.desafioQuality.service;
 
 import com.example.MELI.BootCamp.desafioQuality.DTOs.SizeOfEachRoomDTO;
 import com.example.MELI.BootCamp.desafioQuality.model.Property;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RoomCalculatorServiceImpl implements RoomCalculatorService{
     @Override
     public String getBiggestRoom(Property property) {
